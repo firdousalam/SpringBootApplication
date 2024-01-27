8cf367ef4a42bdc5eee2654094b821bee51edd89
firdous alam <firdous.alam2058@gmail.com>
2024-01-28 02:45:22 +0530

    Springboot Crud Operation

--- demo/src/main/java/com/technophileFirdous/demo/Controller/EmployeeController.java
+++ demo/src/main/java/com/technophileFirdous/demo/Controller/EmployeeController.java
@@ -73,7 +73,7 @@ public class EmployeeController {
 
     }
 
-    @DeleteMapping("employeeToDelete/{empId}")
+    @DeleteMapping("employeeToDelete/{id}")
     public String deleteEmployee(@PathVariable Long id) {
 
         Optional<Employee> emp = employeeRepository.findById(id);
